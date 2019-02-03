dbc6a5782ab282c9debd7dff29956054bc68bd00
Ander <alaboreoca17dw@ikzubirimanteo.com>
2019-02-03 13:09:28 +0100

    correccion

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,30 +16,32 @@
                     
                     @if(Auth::user()->hasRole('admin'))
                     <div>Acceso como administrador</div>
-                    <a href="{{route('admin.index')}}">Ir a la lista de usuarios
-                        @else
+                    <a href="{{route('admin.index')}}">Ir a la lista de usuarios</a>
+                    @else
                         <div>Acceso usuario</div>
                         <a href="{{route('messages.index')}}">Ir a la lista de mensajes
                             <a href="{{route('messages.create')}}">
                                 <br>
                                 <button class="btn btn-primary m-t-15 waves-effect">Nuevo mensaje</button></a>
-                                @endif
+                   
                                 <br>
                                 
                             </div>
                         </div>
+                        @endif
                         <div style="width:400px; height:200px; background-color:{{$color}}">
                             <p>Color: {{$color}}</p>
                             <form action="/home" method="post">
+                                Color:
                                 @csrf
-                                Color: 
+                                
                                 <select name="color">
                                     <option value="red">Rojo</option>
                                     <option value="green">Verde</option>
                                     <option value="white">Blanco</option>
                                 </select>
                                 <br />
-                                <input type="submit" value="Enviar">
+                                <input type="submit" value="Cambiar">
                             </form>
                         </div>
                     </div>
